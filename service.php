83e957ff278fffba40d72d0a142a661f1327fbff
Keito Teranishi <teranishi@undapt.jp>
2014-10-27 02:39:53 +0900

    スタイルの読み込み

--- service.php
+++ service.php
@@ -7,6 +7,9 @@ Template Name:service
 <html>
 <head>
 <meta charset="UTF-8">
+<title><?php bloginfo('name'); ?></title>
+<link rel="stylesheet"  href="<?php bloginfo('stylesheet_url'); ?>">
+<link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/service.css">
 </head>
 <body>
 </body>
