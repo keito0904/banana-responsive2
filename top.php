38cc7abecb8440351ccd22eeb0ceb7b7511f38aa
Keito Teranishi <teranishi@undapt.jp>
2014-10-26 18:30:03 +0900

    スタイルシートの読み込み

--- top.php
+++ top.php
@@ -8,6 +8,7 @@ Template Name:top
 <head>
 <meta charset="UTF-8">
 <title><?php bloginfo('name'); ?></title>
+<link rel="stylesheet"  href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 <body>
 </body>
