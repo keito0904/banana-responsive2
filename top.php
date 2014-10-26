ea7165f46d23255a06ddc8a65a52406ef5fd8f1e
Keito Teranishi <teranishi@undapt.jp>
2014-10-26 20:54:40 +0900

    ナビゲーションの記述

--- top.php
+++ top.php
@@ -17,5 +17,30 @@ Template Name:top
       <h1>Banana Design</h1>
     </div>
   </div>
+  <nav class="top-nav">
+    <ul>
+      <li class="logo">
+        <a href="" class="nav-logo">ロゴ</a>
+      </li>
+      <li class="list-border"></li>
+      <li>
+        <a href="" class="home nav-link">ホーム</a>
+      </li>
+      <li class="list-border"></li>
+      <li>
+        <a href="" class="about nav-link">組織概要</a>
+      </li>
+      <li class="list-border"></li>
+      <li>
+        <a href="" class="service nav-link">事業内容</a>
+      </li>
+      <li class="list-border"></li>
+      <li>
+        <a href="" class="blog nav-link">ブログ</a>
+      </li>
+      <li class="list-border"></li>
+      <div class="clear"></div>
+    </ul>
+  </nav>
 </body>
 </html>
