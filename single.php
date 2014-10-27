a2e0807c30748ed2d3483673c1bd59dd0b4e996a
Keito Teranishi <teranishi@undapt.jp>
2014-10-27 09:13:31 +0900

    スタイルの読み込み

--- single.php
+++ single.php
@@ -2,6 +2,10 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<title><?php bloginfo('name'); ?></title>
+<link rel="stylesheet"  href="<?php bloginfo('stylesheet_url'); ?>">
+<link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/blog.css">
+<link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/single.css">
 </head>
 <body>
 </body>
