58fe44dcd295cb52b9ee043400df993b8fc09714
Keito Teranishi <teranishi@undapt.jp>
2014-10-27 19:37:01 +0900

    スタイルの読み込み

--- category.php
+++ category.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<title><?php bloginfo('name'); ?></title>
+<link rel="stylesheet"  href="<?php bloginfo('stylesheet_url'); ?>">
+<link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/blog.css">
 </head>
 <body>
 </body>
