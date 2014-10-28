7043247e7ce3859a66964e3cf015f08f0c25bdf5
Keito Teranishi <teranishi@undapt.jp>
2014-10-29 04:13:36 +0900

    レスポンシブ対応の記述

--- category.php
+++ category.php
@@ -3,8 +3,13 @@
 <head>
 <meta charset="UTF-8">
 <title><?php bloginfo('name'); ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/dlmenu.css">
 <link rel="stylesheet"  href="<?php bloginfo('stylesheet_url'); ?>">
 <link rel="stylesheet"  href="<?php bloginfo('template_url'); ?>/css/blog.css">
+<script src="<?php bloginfo('template_url'); ?>/js/jquery-2.1.1.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/modernizr.custom.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.dlmenu.js"></script>
 </head>
 <body>
   <?php get_template_part('layouts/header') ?>
